2e7bf638189492f957e3697f87483ec596b839d3
Eli Segal <eli.segal@gmail.com>
2023-01-02 09:08:16 +0200

    wip

--- init.el
+++ init.el
@@ -7,7 +7,8 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages '(doom-themes doom-modeline orderless vertico use-package)))
+ '(package-selected-packages
+   '(which-key doom-themes doom-modeline orderless vertico use-package)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
