570cbe7e50a7ef3dff27b6f113aaabe1e2cc8fcf
Eli Segal <eli.segal@gmail.com>
2023-01-03 17:25:57 +0200

    wip

--- init.el
+++ init.el
@@ -10,7 +10,7 @@
  '(custom-safe-themes
    '("ddffe74bc4bf2c332c2c3f67f1b8141ee1de8fd6b7be103ade50abb97fe70f0c" "be84a2e5c70f991051d4aaf0f049fa11c172e5d784727e0b525565bb1533ec78" "2721b06afaf1769ef63f942bf3e977f208f517b187f2526f0e57c1bd4a000350" "aec7b55f2a13307a55517fdf08438863d694550565dee23181d2ebd973ebd6b8" "f64189544da6f16bab285747d04a92bd57c7e7813d8c24c30f382f087d460a33" "ae426fc51c58ade49774264c17e666ea7f681d8cae62570630539be3d06fd964" "da75eceab6bea9298e04ce5b4b07349f8c02da305734f7c0c8c6af7b5eaa9738" "e3daa8f18440301f3e54f2093fe15f4fe951986a8628e98dcd781efbec7a46f2" "56044c5a9cc45b6ec45c0eb28df100d3f0a576f18eef33ff8ff5d32bac2d9700" "00cec71d41047ebabeb310a325c365d5bc4b7fab0a681a2a108d32fb161b4006" "1a1ac598737d0fcdc4dfab3af3d6f46ab2d5048b8e72bc22f50271fd6d393a00" default))
  '(package-selected-packages
-   '(evil general helpful marginalia which-key doom-themes doom-modeline orderless vertico use-package)))
+   '(consult evil general helpful marginalia which-key doom-themes doom-modeline orderless vertico use-package)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
