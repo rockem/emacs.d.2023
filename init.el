fa199b7665686b5afd695f3776929d130f5c9393
Eli Segal <eli.segal@gmail.com>
2023-01-22 08:52:27 +0200

    wip

--- init.el
+++ init.el
@@ -1,11 +1,18 @@
+;; Eliminate warning when compiling elisp packages
 (when (eq system-type 'darwin) 
   (customize-set-variable 'native-comp-driver-options '("-Wl,-w")))
 
+;; Make gc pauses faster by decreasing the threshold.
+(setq gc-cons-threshold (* 50 1000 1000))
+
+;; Set custom emacs configuratioj go to a separate file
+(setq custom-file (expand-file-name "custom.el" user-emacs-directory))
+(load custom-file 'noerror 'nomessage)
+
+;; Read main org configuration file
 (org-babel-load-file
  (expand-file-name
   "config.org"
   user-emacs-directory))
 
-(setq custom-file (expand-file-name "custom.el" user-emacs-directory))
-(load custom-file 'noerror 'nomessage)
 
